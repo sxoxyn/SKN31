a44d3599327055cdcf9c742d56483a94f069f2b1
Jeon Seoyeon <wjstjdus20@gmail.com>
2026-04-15 17:50:56 +0900

    4/15

---
+++ 01_python/calc.py
@@ -0,0 +1,19 @@
+__version__ = 0.1 # 모듈의 변수
+
+def plus(num1, num2):
+    return num1 + num2
+
+def minus(num1, num2):
+    return num1 - num2
+
+def multipy(num1, num2):
+    return num1 * num2
+
+def divid(num1, num2):
+    return num1 / num2
+
+# 실행 코드
+result = minus(10, 5) # 함수 호출 -> 기본 : 같은 모듈에 정의 된 함수 호출
+print(result)
+
+# python calc.py
---
+++ 01_python/run.py
@@ -0,0 +1,6 @@
+# calc 모듈의 함수들을 호출 -> calc 모듈 import 필요
+import calc 
+
+result = calc.plus(20, 30) # 모듈 이름.사용할 함수() 호출
+print(result)
+print(calc.minus(100, 20))
