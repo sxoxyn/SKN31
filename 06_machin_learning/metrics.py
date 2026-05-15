a7c9d4f4bf8ab90a0ade4c578be08f349027c66e
Jeon Seoyeon <wjstjdus20@gmail.com>
2026-05-15 17:48:34 +0900

    5/15

--- 06_machin_learning/metrics.py
+++ 06_machin_learning/metrics.py
@@ -1,17 +1,68 @@
-# %%writefile 경로 : 셀 내용을 파일로 저장(주피터 노트북 에디터 기능)
 
 ###### 평가 모듈 -> 다양한 평가지표들을 계산/출력하는 함수들가지는 모듈
 import matplotlib.pyplot as plt
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, recall_score, precision_score, f1_score, accuracy_score
+from sklearn.metrics import (confusion_matrix, ConfusionMatrixDisplay, 
+                             recall_score, precision_score, f1_score, accuracy_score,
+                             PrecisionRecallDisplay, average_precision_score, precision_recall_curve,
+                             RocCurveDisplay, roc_auc_score, roc_curve)
 
-__version__ = 1.0
+__version__ = 1.1
+
+def plot_precision_recall_curve(y_proba, pred_proba, estimator_name=None, title=None):
+    """Precision Recall Curve 시각화 함수
+    Args:
+        y_proba: ndarray - 정답
+        pred_proba: 모델이 추정한 양성(Positive-1)일 확률
+        estimator_name: str - 모델 이름. 시각화시 범례에 출력할 모델이름
+        title: str - plot 제목
+    Returns:
+    Raises:"""
+    # ap score 계산
+    ap_score = average_precision_score(y_proba, pred_proba)
+    # thresh 변화에 따른 precision, recall 값들 계산.
+    precision, recall, _ = precision_recall_curve(y_proba, pred_proba)
+    # 시각화
+    disp = PrecisionRecallDisplay(
+        precision, recall, 
+        average_precision=ap_score,  
+        estimator_name=estimator_name
+    )
+    disp.plot()
+    if title:
+        plt.title(title)
+    plt.show()
+
+def plot_roc_curve(y_proba, pred_proba, estimator_name=None, title=None):
+    """ROC Curve 시각화
+    Args:
+        y_proba: ndarray - 정답
+        pred_proba: 모델이 추정한 양성(Positive-1)일 확률
+        estimator_name: str - 모델 이름. 시각화시 범례에 출력할 모델이름
+        title: str - plot 제목
+    Returns:
+    Raises:
+    """
+    ## ROC-AUC score 계산
+    auc_score = roc_auc_score(y_proba, pred_proba)
+    ## Thresh 변화에 따른 TPR(Recall) 과 FPR(위양성율) 계산
+    fpr, tpr, _ = roc_curve(y_proba, pred_proba)
+    ### 시각화
+    disp = RocCurveDisplay(
+        fpr=fpr, tpr=tpr, 
+        estimator_name=estimator_name,
+        roc_auc=auc_score
+    )
+    disp.plot()
+    if title:
+        plt.title(title)
+    plt.show()
 
 def plot_confusion_matrix(y, pred, title=None):
     """Confusion matrix 시각화 함수
     Args:
         y: ndarray - 정답
         pred: ndarray - 모델 추정결과
-        title: str - 출력할 제목. default=None (그래프의 axis)
+        title: str - 출력할 제목. default=None
     Returns:
     Raises:
     """
@@ -22,14 +73,16 @@ def plot_confusion_matrix(y, pred, title=None):
         plt.title(title)
     plt.show()
 
-def print_binary_classification_metrics(y, pred, title=None):
+def print_binary_classification_metrics(y, pred, proba=None, title=None):
     """정확도, 재현율, 정밀도, f1 점수를 계산해서 출력하는 함수
-    Args
+    만약 모델이 추정한 양성의 확률을 전달 받은 경우 average_precision과  roc-auc score도 출력
+    Args:
         y: ndarray - 정답
         pred: ndarray - 모델 추정결과
+        proba: ndarray - 모델이 추정한 양성일 확률값. Default: None
         title: str - 결과에 대한 제목 default=None
-    Returns
-    Raises
+    Returns:
+    Raises:
     """
     if title:
         print(title)
@@ -37,3 +90,6 @@ def print_binary_classification_metrics(y, pred, title=None):
     print("재현율:", recall_score(y, pred))
     print("정밀도:", precision_score(y, pred))
     print("F1 점수:", f1_score(y, pred))
+    if proba is not None:
+        print("Average Precision:", average_precision_score(y, proba))
+        print("ROC-AUC Score:", roc_auc_score(y, proba))
